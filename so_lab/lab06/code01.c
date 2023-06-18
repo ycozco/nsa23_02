b9c1fc1d5a3bbb9c3184d90a0ca4449298bf259a
yosetc <ycozco@unsa.edu.pe>
2023-06-17 21:49:00 -0500

    update indent

--- so_lab/lab06/code01.c
+++ so_lab/lab06/code01.c
@@ -5,33 +5,34 @@ void* funcion(void* p1);
 int c = 0;
 
 int main() {
- pthread_t hilo;
- pthread_attr_t attr;
- int error;
+    pthread_t hilo;
+    pthread_attr_t attr;
+    int error;
 
- pthread_attr_init(&attr);
- error = pthread_create(&hilo, &attr, funcion, NULL);
- if (error != 0) {
- perror("error");
- return(-1);
- }
- int i = 0;
- while (i < 300) {
- c++;
- printf("Padre: %d\n", c);
- i++;
- }
+    pthread_attr_init(&attr);
+    error = pthread_create(&hilo, &attr, funcion, NULL);
+    if (error != 0) {
+        perror("error");
+        return(-1);
+    }
 
- // No call to pthread_join here
- return(0);
+    int i = 0;
+    while (i < 300) {
+        c++;
+        printf("Padre: %d\n", c);
+        i++;
+    }
+
+    // No call to pthread_join here
+    return 0;
 }
 
 void* funcion(void* p1) {
- int i = 0;
- while (i < 300) {
- c--;
- printf("Hijo: %d\n", c);
- i++;
- }
- pthread_exit(0);
+    int i = 0;
+    while (i < 300) {
+        c--;
+        printf("Hijo: %d\n", c);
+        i++;
+    }
+    pthread_exit(0);
 }
