6fd8ebf91f223ed7ac8956ea4a24e8127db71b79
yosetc <ycozco@unsa.edu.pe>
2023-06-17 21:46:01 -0500

    lab06

---
+++ so_lab/lab06/code01.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void* funcion(void* p1);
+int c = 0;
+
+int main() {
+ pthread_t hilo;
+ pthread_attr_t attr;
+ int error;
+
+ pthread_attr_init(&attr);
+ error = pthread_create(&hilo, &attr, funcion, NULL);
+ if (error != 0) {
+ perror("error");
+ return(-1);
+ }
+ int i = 0;
+ while (i < 300) {
+ c++;
+ printf("Padre: %d\n", c);
+ i++;
+ }
+
+ // No call to pthread_join here
+ return(0);
+}
+
+void* funcion(void* p1) {
+ int i = 0;
+ while (i < 300) {
+ c--;
+ printf("Hijo: %d\n", c);
+ i++;
+ }
+ pthread_exit(0);
+}
