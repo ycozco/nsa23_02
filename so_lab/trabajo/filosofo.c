f01fd59c2f7d6882ae95c0a953295718b239227e
yosetc <ycozco@unsa.edu.pe>
2023-07-01 05:42:29 -0500

    test 07.01

--- so_lab/trabajo/filosofo.c
+++ so_lab/trabajo/filosofo.c
@@ -1,31 +1,40 @@
 #include <iostream>
 #include <thread>
 #include <semaphore.h>
+#include <chrono>
 
 #define NUM_FILOSOFOS 5
-#define IZQUIERDA (i + NUM_FILOSOFOS - 1) % NUM_FILOSOFOS
-#define DERECHA (i + 1) % NUM_FILOSOFOS
+#define IZQUIERDA(i) ((i + NUM_FILOSOFOS - 1) % NUM_FILOSOFOS)
+#define DERECHA(i) ((i + 1) % NUM_FILOSOFOS)
 
 sem_t cubierto[NUM_FILOSOFOS];
 sem_t inicioSemaforo;
 
 void meditar() {
     // Lógica para meditar
+    // Aquí puedes implementar el código correspondiente a la meditación de los filósofos
 }
 
 void alimentarse(int id) {
     std::cout << "Filósofo " << id << " está tratando de obtener los cubiertos." << std::endl;
 
     // Solicitar los cubiertos
-    sem_wait(&cubierto[IZQUIERDA]);
-    sem_wait(&cubierto[DERECHA]);
+    sem_wait(&cubierto[IZQUIERDA(id)]);
+    sem_wait(&cubierto[DERECHA(id)]);
 
     std::cout << "Filósofo " << id << " tiene los cubiertos y está comiendo." << std::endl;
-    // Lógica para alimentarse
+    auto start_time = std::chrono::steady_clock::now();
+    // Simulación de la alimentación durante 1 segundo
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    auto end_time = std::chrono::steady_clock::now();
+
+    auto elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    std::cout << "Filósofo " << id << " ha terminado de comer. Tiempo de uso de los cubiertos: "
+              << elapsed_time.count() << " ms" << std::endl;
 
     // Liberar los cubiertos
-    sem_post(&cubierto[DERECHA]);
-    sem_post(&cubierto[IZQUIERDA]);
+    sem_post(&cubierto[DERECHA(id)]);
+    sem_post(&cubierto[IZQUIERDA(id)]);
 }
 
 void filosofo(int id) {
@@ -43,16 +52,20 @@ void filosofo(int id) {
 int main() {
     std::thread filosofos[NUM_FILOSOFOS];
 
+    // Inicializar los semáforos de los cubiertos
     for (int i = 0; i < NUM_FILOSOFOS; i++) {
         sem_init(&cubierto[i], 0, 1);
     }
 
+    // Inicializar el semáforo de inicio
     sem_init(&inicioSemaforo, 0, 1);
 
+    // Crear los hilos de los filósofos
     for (int i = 0; i < NUM_FILOSOFOS; i++) {
         filosofos[i] = std::thread(filosofo, i);
     }
 
+    // Esperar a que todos los hilos terminen
     for (int i = 0; i < NUM_FILOSOFOS; i++) {
         filosofos[i].join();
     }
