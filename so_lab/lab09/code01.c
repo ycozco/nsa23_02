22b7786f443a86cf5815041ef45bc25fb161f9ec
ycozco <ycozco@unsa.edu.pe>
2023-07-23 22:06:02 -0500

    update codeo1

--- so_lab/lab09/code01.c
+++ so_lab/lab09/code01.c
@@ -3,9 +3,9 @@
 #include <errno.h>
 #include <semaphore.h>
 #include <pthread.h>
-#include <unistd.h> // Incluir la biblioteca para la función sleep
+#include <unistd.h>
 
-#define SUMSIZE 20 // Aumentar el número de iteraciones para ambos hilos
+#define SUMSIZE 20
 #define BUFSIZE 8
 
 int buffer[BUFSIZE];
@@ -13,10 +13,10 @@ sem_t llenos;
 sem_t vacios;
 
 static void *productor(void *arg1) {
-    int i, entra, producir;
+    int i, entra;
     entra = 0;
     for (i = 1; i <= SUMSIZE; i++) {
-        while ((producir = getchar()) == '\n');
+        char producir = 'A' + (i % 26); // Genera automáticamente caracteres 'A' a 'Z'
         sem_wait(&vacios);
         buffer[entra] = producir;
         entra = (entra + 1) % BUFSIZE;
@@ -28,14 +28,14 @@ static void *productor(void *arg1) {
 }
 
 static void *consumidor(void *arg2) {
-    int i, sale, consumir;
+    int i, sale;
     sale = 0;
     for (i = 1; i <= SUMSIZE; i++) {
         sem_wait(&llenos);
         printf("\tConsumidor <--\t");
         sleep(1); // El consumo se demora 1 segundo
         sem_post(&vacios);
-        consumir = buffer[sale];
+        char consumir = buffer[sale];
         sale = (sale + 1) % BUFSIZE;
         printf(" %c\n", consumir);
     }
@@ -67,5 +67,6 @@ int main() {
 
     return 0;
 }
+
 // path: so_lab\lab09\code01.c
 // how to exec: gcc code01.c -o code01 -lpthread
